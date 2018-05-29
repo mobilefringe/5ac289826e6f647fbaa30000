5a2289b27b23a86a36b23f4a5b4ea4a8d89a4260
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-05-29 16:33:40 -0400

    St. Laurent Shopping Centre 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -221,8 +221,7 @@
             methods: {
                 loadData: async function() {
                     try {
-                        // avoid making LOAD_META_DATA call for now as it will cause the entire Promise.all to fail since no meta data is set up.
-                        let results = await Promise.all([this.$store.dispatch("getData", "banners"), this.$store.dispatch("getData", "feature_items"), this.$store.dispatch("getData", "popups"), this.$store.dispatch('LOAD_PAGE_DATA', {url: "http://stlaurent.mallmaverick.com/api/v3/stlaurent/social.json"})]);
+                        let results = await Promise.all([this.$store.dispatch("getData", "banners"), this.$store.dispatch("getData", "feature_items"), this.$store.dispatch("getData", "popups"), this.$store.dispatch('LOAD_PAGE_DATA', {url: "https://stlaurent.mallmaverick.com/api/v4/stlaurent/social.json"})]);
                         return results;
                     } catch (e) {
                         console.log("Error loading data: " + e.message);
